84df2d250f4349e0238e423bd34de5a1fb7e6799
Jin Jae-yeon <daramkun@live.com>
2018-10-19 16:45:15 +0900

    Adding Boss's bullets

---
+++ Assets/Scripts/Enemy/Bullets/SquirrelBulletEmitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelBulletEmitter : MonoBehaviour {
+
+	GameObject bullet;
+	float elapsedTime;
+	float angle;
+
+	// Use this for initialization
+	void Start () {
+		bullet = Resources.Load<GameObject> ( "Prefabs/Enemies/Bullets/SquirrelBullet" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+
+		if ( elapsedTime >= 0.2f )
+		{
+			Instantiate ( bullet ).GetComponent<EnemyBulletController> ().Angle = angle;
+			angle += 10;
+			elapsedTime -= 0.2f;
+		}
+	}
+}
